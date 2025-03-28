296e552bfb98eeb7bc0d43bf77f920146aa7ec21
Mohey Karam <moheykaramdev@gmail.com>
2025-03-28 23:55:37 +0200

    style: added different shades to the colors

--- src/modules/store/pages/HomePage.jsx
+++ src/modules/store/pages/HomePage.jsx
@@ -6,7 +6,7 @@ function Home() {
       <h1>
         Deez
       </h1>
-      <h2 className="text-secondary-dark bg-amber-50 text-8xl">Nuts</h2>
+      <h2 className="text-secondary-800 bg-amber-50 text-8xl">Nuts</h2>
       <h3 className="text-accent text-7xl">Ya Khalid</h3>
       <h3 className="text-primary text-7xl">Ya Khalid</h3>
     </Container>
