cbdea0df86868d0df0453a09c5dc200c67ae0d94
Mohey Karam <moheykaramdev@gmail.com>
2025-03-31 20:39:09 +0200

    fixed error due to svg attributes in jsx

--- src/modules/_shared/components/header.jsx
+++ src/modules/_shared/components/header.jsx
@@ -60,10 +60,9 @@ function Header() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M4 6l16 0" />
