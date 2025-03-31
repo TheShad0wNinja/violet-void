530acd8f0c65e10d9916a44ce09545b30b403120
Mohey Karam <moheykaramdev@gmail.com>
2025-03-31 20:56:28 +0200

    fixed router generation lack of keys - Added ability to add parent routes with paths

--- src/routes.jsx
+++ src/routes.jsx
@@ -22,21 +22,39 @@ const routesLinks = [
   }
 ];
 
-function getLinks(links) {
-  return links.map(link => {
-    if (link.children) {
-      return <Route element={link.element}>{getLinks(link.children)}</Route>;
-    }
-    return <Route path={link.path} element={link.element} />;
+function getLinks(links, keyPrefix = "") {
+  return links.map((link, idx) => {
+    const key = `${keyPrefix}/${idx}`;
+
+    if (link.children && link.path && link.element)
+      return (
+        <Route key={key} path={link.path} element={link.element}>
+          {getLinks(link.children, key)}
+        </Route>
+      );
+
+    if (link.children && link.path)
+      return (
+        <Route key={key} path={link.path}>
+          {getLinks(link.children, key)}
+        </Route>
+      );
+
+    if (link.children && link.element)
+      return (
+        <Route key={key} element={link.element}>
+          {getLinks(link.children, key)}
+        </Route>
+      );
+
+    return <Route key={key} path={link.path} element={link.element} />;
   });
 }
 
 export default function VVRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        {getLinks(routesLinks)}
-      </Routes>
+      <Routes>{getLinks(routesLinks)}</Routes>
     </BrowserRouter>
   );
 }
