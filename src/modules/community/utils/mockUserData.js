f0f6cf12860cc1cc03aaddb63217fd0b9db9078a
Electronicle <zizospider2004@gmail.com>
2025-04-03 15:19:10 +0200

    Added Mock Data to use for Artists

---
+++ src/modules/community/utils/mockUserData.js
@@ -0,0 +1,28 @@
+const artists = [
+  {
+    name: "Chun Lo",
+    handle: "belgeist",
+    profilePic:
+      "https://cdnb.artstation.com/p/users/avatars/000/002/295/large/25d34b3bcf966aadf0d9e5e08fc2bc3e.jpg?1437762114",
+    art: "https://cdnb.artstation.com/p/assets/images/images/010/099/811/large/chun-lo-venom.jpg?1522588103",
+    game: "Marvel Rivals"
+  },
+  {
+    name: "Chun Lo",
+    handle: "belgeist",
+    profilePic:
+      "https://cdnb.artstation.com/p/users/avatars/000/002/295/large/25d34b3bcf966aadf0d9e5e08fc2bc3e.jpg?1437762114",
+    art: "https://pbs.twimg.com/media/GiVjcuNXcAA9xdM?format=jpg&name=large",
+    game: "Marvel Rivals"
+  },
+  {
+    name: "Chick L",
+    handle: "CHICK_LLL",
+    profilePic: "https://pbs.twimg.com/profile_images/1877047310053781504/ISfypusy_400x400.jpg",
+    art: "https://pbs.twimg.com/media/GhalvzMboAAOMyi?format=jpg&name=large",
+    game: "Dead Cells"
+  }
+];
+export function getArtists() {
+  return artists;
+}
