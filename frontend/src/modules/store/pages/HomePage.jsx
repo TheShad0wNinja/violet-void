9e8a61d389d256f00dd71ef5a96f53a5a7b0b5bd
Electronicle <zizospider2004@gmail.com>
2025-05-05 00:11:43 +0300

    Removed Artworks from Homepage cuz bugs out

--- frontend/src/modules/store/pages/HomePage.jsx
+++ frontend/src/modules/store/pages/HomePage.jsx
@@ -1,40 +1,27 @@
-import {
-  Carousel,
-  Container,
-  Divider,
-  GamesHolder,
-  SkeletonBox,
-  Title
-} from "@modules/_shared/App";
-import { getGamesList, getGamesPageData, getSimilarGamesData } from "../utils/mockData";
-import { getShuffledArtworks } from "@modules/community/utils/mockUserData";
-import { ArtworkComponent, MoreButton } from "@modules/community/App";
+import { Carousel, Container, GamesHolder } from "@modules/_shared/App";
+import { getGamesList, getGamesPageData } from "../utils/mockData";
 
-import { useState } from "react";
-import GameCardFullData from "../components/GameCardFullData";
 import CategoriesGrid from "../components/CategoriesGrid";
 import GameSection from "../components/GameSection";
 
 const games = getGamesPageData();
 const imagesgames = getGamesList();
 
-const artworks = getShuffledArtworks();
-
 function Home() {
   return (
     <>
       <Container>
-				<Carousel 
-					items={imagesgames}
-					itemsPerPage={1}
-					infiniteLoop={true}
-					renderItem={(panel) => <CarouselItem panel={panel} />}
-					containerClass="max-h-[500px]"
-				/>
+        <Carousel
+          items={imagesgames}
+          itemsPerPage={1}
+          infiniteLoop={true}
+          renderItem={panel => <CarouselItem panel={panel} />}
+          containerClass="max-h-[500px]"
+        />
         <div className="p-5">
-					<div className="hidden md:block">
-						<GamesHolder Sectionname="Trending games" games={games} />
-					</div>
+          <div className="hidden md:block">
+            <GamesHolder Sectionname="Trending games" games={games} />
+          </div>
           <GamesHolder Sectionname="Lastest Games" type2games games={games} smallerHeight />
           <div className="mt-5 mb-3 flex flex-col gap-2">
             <h1 className="text-2xl font-bold">Browse by genre</h1>
@@ -45,7 +32,6 @@ function Home() {
             <GameSection games={games} sectionName="Under $20" />
             <GameSection games={games} sectionName="On sale" />
           </div>
-          <ArtworkComponent />
         </div>
       </Container>
     </>
@@ -53,7 +39,11 @@ function Home() {
 }
 function CarouselItem({ panel }) {
   return (
-		<img src={panel.bannerImageUrl} alt={panel.id+"_carousel"} className="w-full h-full object-cover rounded-xl" />
+    <img
+      src={panel.bannerImageUrl}
+      alt={panel.id + "_carousel"}
+      className="h-full w-full rounded-xl object-cover"
+    />
   );
 }
 
